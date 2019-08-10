a73d99d020c6e793047e8f26c1731ced486b7cee
梦缘 <hongwen.chw@antfin.com>
2019-08-10 23:06:48 +0800

    赛车飞艇24，前判680，后判67890

--- src/main/java/cn/cherish/dubo/dubo/service/AbstractService.java
+++ src/main/java/cn/cherish/dubo/dubo/service/AbstractService.java
@@ -9,6 +9,7 @@ import cn.cherish.dubo.dubo.util.AlertOverUtils;
 import cn.cherish.dubo.dubo.util.DuboUtils;
 import cn.cherish.dubo.dubo.util.DuboUtils.History;
 import cn.cherish.dubo.dubo.util.IntUtils;
+import com.google.common.collect.Sets;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
@@ -16,6 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.stream.Collectors;
 import lombok.Builder;
@@ -219,14 +221,119 @@ public abstract class AbstractService {
 
     }
 
-    protected void printArr(int len, boolean[][] evenOddBool, int evenOddTickNum) {
-        for (int k = 0; k < evenOddTickNum; k++) {
-            for (int i = 0; i < len; i++) {
-                System.out.print((evenOddBool[k][i] ? 1 : 0) + " ");
+    protected int data24CountFailAlert = 6;
+    protected void data24(List<Term> sortedTerms){
+        int size = sortedTerms.size();
+        int columnLen = sortedTerms.get(0).getTermDataArr().length;
+
+        Set<Integer> data67890 = Sets.newHashSet(6, 7, 8, 9, 10);
+        Set<Integer> data24 = Sets.newHashSet(2, 4);
+        Set<Integer> data680 = Sets.newHashSet(6, 8, 10);
+
+        // 遍历每列
+        for (int c = 0; c < columnLen / 3; c++) {
+            // 连续错误的次数
+            int countFail = 0;
+
+            // 遍历每行 首末最后处理
+            for (int r = 1; r <= size - 2; r++) {
+
+                // 当前行
+                Term curTerm = sortedTerms.get(r);
+                Long termNum = curTerm.getTermNum();
+                Integer[] termDataArr = curTerm.getTermDataArr();
+                Integer termData = termDataArr[c * 3];
+                // 0:小，1:大
+                Integer bigSmall = termDataArr[c * 3 + 2];
+
+                // 判断匹配24
+                if (!data24.contains(termData)) {
+                    continue;
+                }
+
+                // 前面一行
+                Integer[] preTermDataArr = sortedTerms.get(r - 1).getTermDataArr();
+                Integer preTermData = preTermDataArr[c * 3];
+
+                // 后面一行
+                Integer[] nextTermDataArr = sortedTerms.get(r + 1).getTermDataArr();
+                Integer nextTermData = nextTermDataArr[c * 3];
+                Integer nextBigSmall = nextTermDataArr[c * 3 + 2];
+
+                // 前面一行的判断
+                if (data680.contains(preTermData)) {
+                    if (1 == nextBigSmall) {
+                        countFail = 0;
+                    } else {
+                        countFail++;
+                    }
+                }
+
+                // 后面一行的判断
+                if (data67890.contains(nextTermData)) {
+                    // 判断是否最后一行
+                    if (r == size - 2) {
+                        // 快要告警了！！！！！
+                        break;
+                    }
+
+                    Integer next2BigSmall = sortedTerms.get(r + 2).getTermDataArr()[c * 3 + 2];
+                    if (1 == next2BigSmall) {
+                        countFail = 0;
+                    } else {
+                        countFail++;
+                    }
+
+                }
+                log.info("{} c={},r={},termNum={},countFail={}", name, c, r, termNum, countFail);
+
+            }// 遍历每行 首末最后处理
+
+            // 是都达到预警阈值
+            if (countFail < data24CountFailAlert) {
+                continue;
             }
-            System.out.println();
-        }
-        System.out.println();
+
+            // 最新一行
+            Integer[] lastTermDataArr = sortedTerms.get(size - 1).getTermDataArr();
+            Integer lastTermData = lastTermDataArr[c * 3];
+            // 0:小，1:大
+            Integer lastBigSmall = lastTermDataArr[c * 3 + 2];
+
+            boolean isAlert = false;
+
+            // 24 该判断前一行是大
+            if (data24.contains(lastTermData)) {
+                if (1 == sortedTerms.get(size - 2).getTermDataArr()[c * 3 + 2]) {
+                    // do alert
+                    isAlert = true;
+                }
+            }
+            // 大  该判断前一行是24
+            if (1 == lastBigSmall) {
+                if (data24.contains(sortedTerms.get(size - 2).getTermDataArr()[c * 3])) {
+                    // do alert
+                    isAlert = true;
+                }
+            }
+
+            if (isAlert) {
+
+                String alertContent = ""
+                    + "种类：" + name + "24，前判680，后判67890\r\n"
+                    + "错误：" + countFail + "次\r\n"
+                    + "列号：" + (c + 1) + "\n"
+                    + "时间：" + new Date() + "\n"
+                    + "";
+                AbstractService.AlertTask alertTask = AbstractService.AlertTask.builder()
+                    .title(name)
+                    .content(alertContent)
+                    .url(url)
+                    .build();
+                alertTaskQueue.add(alertTask);
+            }
+
+        }// 遍历每列
     }
 
     @Data
